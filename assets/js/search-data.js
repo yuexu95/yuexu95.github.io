538a2082b3d7617d38e208f067cef1d433838677
yuexu95 <93499467+yuexu95@users.noreply.github.com>
2026-03-27 23:41:12 +0000

    Deploying to gh-pages from @ yuexu95/yuexu95.github.io@ad3ef2a56ddf56a9f657e7c93ca42932bf21cbf7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -95,30 +95,4 @@ ninja.data = [{
         handler: () => {
           window.open("https://twitter.com/YueXu1995", "_blank");
         },
-      },{
-      id: 'light-theme',
-      title: 'Change theme to light',
-      description: 'Change the theme of the site to Light',
-      section: 'Theme',
-      handler: () => {
-        setThemeSetting("light");
-      },
-    },
-    {
-      id: 'dark-theme',
-      title: 'Change theme to dark',
-      description: 'Change the theme of the site to Dark',
-      section: 'Theme',
-      handler: () => {
-        setThemeSetting("dark");
-      },
-    },
-    {
-      id: 'system-theme',
-      title: 'Use system default theme',
-      description: 'Change the theme of the site to System Default',
-      section: 'Theme',
-      handler: () => {
-        setThemeSetting("system");
-      },
-    },];
+      },];
