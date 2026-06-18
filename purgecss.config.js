6be9dc7659a000dd729e61835bbd39f8b5875d42
yuexu95 <cpuxuyue@gmail.com>
2026-06-18 17:46:22 -0500

    Add safelist configuration to purgecss for improved CSS handling

--- purgecss.config.js
+++ purgecss.config.js
@@ -3,4 +3,8 @@ module.exports = {
   css: ["_site/assets/css/*.css"],
   output: "_site/assets/css/",
   skippedContentGlobs: ["_site/assets/**/*.html"],
+  safelist: {
+    pattern: /^(ti|ti-|ai|ai-|fa|fa-|fas|far|fab|scholar-)/,
+    greedy: true,
+  },
 };
